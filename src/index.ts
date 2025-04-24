cd6d4071958fb1b3dcc240900f6080e566a56530
Raaid17 <134477393+Raaid17@users.noreply.github.com>
2025-04-25 00:10:24 +0800

    Fix index.ts exports and rebuild

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Discord } from './nodes/Discord/Discord.node';
+import { DiscordTrigger } from './nodes/Discord/DiscordTrigger.node';
+import { DiscordApi } from './credentials/DiscordApi.credentials';
+
+export {
+	Discord,
+	DiscordTrigger,
+	DiscordApi,
+};
